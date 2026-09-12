66f1ed5e58d4ea3c24e28cc67e602036f354087d
mrayoubchafiki <ayoubchafiki045@gmail.com>
2026-09-12 09:39:24 +0100

    Update day_3.py

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,1 +1,88 @@
+#Day 3 of 30DaysOfPython
+age  = 21
+height = 10
+complex_number = 2+5j
+base = int(input('enter base'))
+height = float(input('enter height'))
+area = 0.5*base*height
+print(area)
+a = int(input('enter side a :'))
+b = int(input('enter side b :'))
+c = int(input('enter side c :'))
+total = a+b+c
+print('The perimeter of the triangle is ', total)
+legth =  int(input('enter legth :'))
+width =   int(input('enter width :'))
+area = legth*width
+print('area is ', aerea)
+perimeter = = 2 x (length + width)
+print('perimeter is ' , permiter)
+r= int(input('enter raduis :'))
+pi = 3.14
+area_of_circle = pi*r*r
+print('area of circle is ', area_of_circle)
+circum_of_circle = 2 * pi * r
+print(circum_of_circle)
+r = int(input('enter a raduis : '))
+area = pow(r,2) * pi
+print('area is ', area)
+Slope1 = 2
+x-intercept = 1
+y-intercept = -2
 
+Slope2 = 2
+Euclidean ≈ 8.94
+Slope1 == Slope2
+
+x = int(input("Enter x: "))
+y = x*x + 6*x + 9
+
+print("y =", y)
+
+if y == 0:
+    print("y is 0!")
+
+python = "python"
+dragon = "dragon"
+print('legth of python is ',len(python))
+print('legth of dragon is ',len(dragon))
+if len(python) == len(dragon) :
+    print(True)
+else :
+    print(False)
+
+print(" 'on' in python and dragon :" , 'on' in 'python' and 'on' in 'dragon')
+print("I hope this course is not full of jargon :", 'jargon' in 'I hope this course is not full of jargon ')
+
+python = "python"
+p = len(python)
+p = float(p)
+p = str(p)
+print(p)
+
+number = int(input("enter a number : "))
+if number %2 == 0 : 
+    print("Number is Even")
+else :
+        print("Number is Odd")
+
+s = 2.7
+s = int(s)
+print("check is ", 7//3 == s)
+
+print("check is : ", type('10') == type(10))
+a = 9.8
+a = int(a)
+print("check is : ", a == 10 )
+
+hours = float(input("Enter hours : "))
+rate = float(input("Enter rate per hour :"))
+pay = hours*rate
+print("Your weekly earning is ", pay)
+
+years = int(input("Enter number of years you have lived:"))
+seconds = years*365*24*60*60
+print("You have lived for ",seconds," seconds")
+
+for i in range(1, 6):
+    print(i, i**0, i, i**2, i**3)
